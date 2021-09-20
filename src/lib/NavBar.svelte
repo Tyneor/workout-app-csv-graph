7977b4992575dd64876e8874824828a4f54244c6
Tyneor <etienne.quarez@gmail.com>
2021-09-20 23:42:12 +0200

    moved pages params in variable in navbar

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,74 +1,72 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import Icon from '$lib/Icon.svelte';
+	import { mdiCalculator, mdiChartTimelineVariant, mdiHistory, mdiHome } from '@mdi/js';
+
+	const pages = [
+		{ path: '/', title: 'Home', icon: mdiHome },
+		{ path: '/history', title: 'History', icon: mdiHistory },
+		{ path: '/charts', title: 'Charts', icon: mdiChartTimelineVariant },
+		{ path: '/calculator', title: 'calculator', icon: mdiCalculator }
+	];
 </script>
 
 <nav>
 	<ul>
-		<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-		<li class:active={$page.path === '/history'}>
-			<a sveltekit:prefetch href="/history">History</a>
-		</li>
-		<li class:active={$page.path === '/charts'}>
-			<a sveltekit:prefetch href="/charts">Charts</a>
-		</li>
-		<li class:active={$page.path === '/calculator'}>
-			<a sveltekit:prefetch href="/calculator">Calculator</a>
-		</li>
+		{#each pages as { path, title, icon }, i}
+			<li class:active={$page.path === path}>
+				<a sveltekit:prefetch href={path}><Icon path={icon} /></a>
+			</li>
+		{/each}
 	</ul>
 </nav>
 
-<style>
+<style lang="scss">
 	nav {
-		position: sticky;
+		position: fixed;
 		bottom: 0;
+		width: 100%;
 		display: flex;
 		justify-content: center;
-	}
+		background: linear-gradient(rgba(0, 0, 0, 0) 0%, #111111d0 30%, var(--bg-color) 100%);
 
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background-size: contain;
-	}
+		ul {
+			position: relative;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			gap: 1rem;
+			padding-bottom: 0.5rem;
+			list-style: none;
 
-	li {
-		position: relative;
-		height: 100%;
-	}
+			li {
+				position: relative;
+				height: 100%;
 
-	li.active::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--accent-color);
-	}
+				a {
+					transition: color 0.2s linear;
+					color: white;
 
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 1em;
-		color: var(--heading-color);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
+					&:hover,
+					&:active {
+						color: var(--accent-color);
+					}
+				}
 
-	a:hover {
-		color: var(--accent-color);
+				&.active::after {
+					--size: 6px;
+					content: ' ';
+					position: relative;
+					display: block;
+					left: 50%;
+					transform: translateX(-50%);
+					bottom: 0;
+					width: var(--size);
+					height: var(--size);
+					border-radius: 100px;
+					background-color: var(--accent-color);
+				}
+			}
+		}
 	}
 </style>
