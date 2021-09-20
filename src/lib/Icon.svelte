73544ec1baa38ebf42f0cc6119fd9bc54aa64324
Tyneor <etienne.quarez@gmail.com>
2021-09-20 22:35:58 +0200

    added Icon component and sass support

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let size = 24;
+	export let path: string;
+</script>
+
+<svg width={size} height={size} viewBox="0 0 24 24">
+	<path d={path} fill="currentColor" />
+</svg>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
 import preprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
+const { scss } = preprocess;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess(scss()),
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
